a74d07253877fa0cea7dc715704b64c383a0a847
Anu1601CS <anuragvns1111@gmail.com>
2018-12-24 21:48:56 +0530

    config

---
+++ configuration.php
@@ -0,0 +1,15 @@
+<?php
+		class Config {
+			public $username = 'anu1601cs';
+			public $useremail = 'anuragvns1111@gmail.com';
+			public $sitename = 'iitpConnect';
+			public $debug = true;
+			public $dbtype = 'mysqli';
+			public $host = 'localhost';
+			public $dbusername = 'b7d83e2580260c';
+			public $dbpassword = '0fc606f7';
+			public $db = 'iitpca1';
+			public $secret = '39db0fbb14c0f11e97da40d08fd93411';
+			public $error_reporting = 'default';
+			public $baseurl = '/anurag/hero/iitpConnect/';
+		}
